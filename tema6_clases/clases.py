b220bbcfac46a058909dad525c893695459eae95
Javier Viesca <viesca@gmail.com>
2022-06-06 09:09:57 +0200

    Python OpenBootcamp

--- tema6_clases/clases.py
+++ tema6_clases/clases.py
@@ -3,10 +3,12 @@ class Dino:
     #con _ delante NO MODIFICAR
     _encendido = False
 
+    #funcion Apaga
     def apaga(self):
         self._encendido = False
         print("Luz apagada")
-    
+
+    #funcion Enciende
     def enciende(self):
         self._encendido = True
         print("Luz Encendida")
@@ -26,3 +28,17 @@ print(d._encendido)
 
 d.enciende()
 print(d._encendido)
+
+#Clases estaticas. Sin instanciar la clase están en la misma area de memoria
+class Estatica:
+    numero = 1
+
+    def incrementa():
+        Estatica.numero+=1
+
+Estatica.incrementa()
+print(Estatica.numero)
+
+Estatica.incrementa()
+print(Estatica.numero)
+
