a66c071caf02f85da4fadb04eab28e996431852a
Javier Viesca <viesca@gmail.com>
2022-06-11 20:27:48 +0200

    Python OpenBootcamp

---
+++ tema7_modulos/ejercicio2/main.py
@@ -0,0 +1,12 @@
+import time
+
+
+def aCasa():
+    hora = (int)(time.strftime('%H'))
+    if hora >= 7 and hora < 19:
+        print("Te quedan unas", 19 - hora, "horas para ir a casa")
+    else:
+        print("Es hora de irse a casa")
+  
+
+aCasa()
