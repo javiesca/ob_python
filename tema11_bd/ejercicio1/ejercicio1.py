60ea858e6a5e37753803b3a7936db67fb34593ca
Viesca <viesca@gmail.com>
2022-06-23 15:10:11 +0200

    Python OpenBootcamp

--- tema11_bd/ejercicio1/ejercicio1.py
+++ tema11_bd/ejercicio1/ejercicio1.py
@@ -23,24 +23,22 @@ def menu():
 def inserta_alumno():
     numero_alumnos = int(input("Cuantos Alumnos deseas insertar?"))
     contador = 0
+    conn = sqlite3.connect('miBasedeDatos.db', isolation_level=None)
 
     while contador < numero_alumnos:
         id = input("ID del usuario: ")
         nombre = input("Inserta el nombre del Alumno: ")
         apellido = input("Inserta el apellido del Alumno: ")
-
+        cursor = conn.cursor()
+        query = f"INSERT INTO Alumnos (id, nombre, apellido) VALUES({id}, '{nombre}', '{apellido}')"
+        cursor.execute(query)
+        conn.commit()
+        cursor.close()
         contador = contador + 1
 
-    print(f"Se han insertado {contador} alumnos en la base de datos.")
 
-    conn = sqlite3.connect('miBasedeDatos.db', isolation_level=None)
-    cursor = conn.cursor()
-
-    query = f"INSERT INTO Alumnos (id, nombre, apellido) VALUES({id}, '{nombre}', '{apellido}')"
-    rows = cursor.execute(query)
-
-    cursor.close()
     conn.close()
+    print(f"Se han insertado {contador} alumnos en la base de datos.")
     main()
 
 
